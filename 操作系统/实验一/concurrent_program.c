d333a8ef89aa28edae8e3afc9e3ac78c91ca4374
InverseDa <inverse_boom@outlook.com>
2023-02-26 01:57:06 +0800

    init

---
+++ 操作系统/实验一/concurrent_program.c
@@ -0,0 +1,42 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+
+int tprintf(const char* fmt, ...) {
+    va_list args;
+    struct tm *tstruct;
+    time_t tsec;
+    tsec = time(NULL);
+    tstruct = localtime(&tsec);
+    printf("%02d:%02d:%02d: %05d| ", tstruct->tm_hour, tstruct->tm_min, tstruct->tm_sec, getpid());
+    va_start(args, fmt);
+    return vprintf(fmt, args);
+}
+
+int main() {
+    int i = 0, j = 0;
+    pid_t pid;
+    printf("Hello from Parent Process, PID is %d.\n", getpid());
+    
+    pid = fork();
+    
+    if (pid == 0) {
+        sleep(1);
+        for(int i = 0; i < 3; i++) {
+            printf("Hello from Child Process %d. %d times\n", getpid(), i + 1);
+            sleep(1);
+        }
+    } else if (pid != -1) {
+        tprintf("Parent forked one child process--%d.\n", pid);
+        tprintf("Parent is waiting for child to exit.\n");
+        waitpid(pid, NULL, 0);
+        tprintf("Child Process has exited.\n");
+        tprintf("Parent had exited.\n");
+    } else tprintf("Everything was done without error.\n");
+
+    return 0;
+}
