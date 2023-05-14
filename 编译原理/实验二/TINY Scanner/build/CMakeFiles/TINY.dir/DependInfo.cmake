3bd6ee9456a592fc4d4b3edb7311c7589475fe37
InverseDa <inverse_boom@outlook.com>
2023-05-14 20:56:00 +0800

    init

---
+++ 编译原理/实验二/TINY Scanner/build/CMakeFiles/TINY.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/miaokeda/Documents/notes/编译原理/实验二/TINY Scanner/src/main.c" "CMakeFiles/TINY.dir/src/main.c.o" "gcc" "CMakeFiles/TINY.dir/src/main.c.o.d"
+  "/Users/miaokeda/Documents/notes/编译原理/实验二/TINY Scanner/src/scan.c" "CMakeFiles/TINY.dir/src/scan.c.o" "gcc" "CMakeFiles/TINY.dir/src/scan.c.o.d"
+  "/Users/miaokeda/Documents/notes/编译原理/实验二/TINY Scanner/src/util.c" "CMakeFiles/TINY.dir/src/util.c.o" "gcc" "CMakeFiles/TINY.dir/src/util.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 编译原理/实验二/TINY Scanner/build/CMakeFiles/TINY.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/TINY.dir/src/main.c.o"
+  "CMakeFiles/TINY.dir/src/main.c.o.d"
+  "CMakeFiles/TINY.dir/src/scan.c.o"
+  "CMakeFiles/TINY.dir/src/scan.c.o.d"
+  "CMakeFiles/TINY.dir/src/util.c.o"
+  "CMakeFiles/TINY.dir/src/util.c.o.d"
+  "TINY"
+  "TINY.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/TINY.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
